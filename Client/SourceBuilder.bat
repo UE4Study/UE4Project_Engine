ad20bdd8cd90803cca0ad39b012cb7002e79fbf4
kmu1205 <kmu1205@naver.com>
2017-05-18 17:27:04 +0900

    [소스빌더] 소스빌더.

--- Client/SourceBuilder.bat
+++ Client/SourceBuilder.bat
@@ -1,3 +1,3 @@
-%cd%\..\Engine\4.14Release\Engine\Binaries\DotNet\UnrealBuildTool.exe -projectfiles -project="%cd%\Client.uproject" -game -rocket -progress -2015
+%cd%\..\Engine\4.14.3\Engine\Binaries\DotNet\UnrealBuildTool.exe -projectfiles -project="%cd%\Client.uproject" -game -rocket -progress -2015
 
 pause
