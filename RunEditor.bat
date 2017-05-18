982ec59bfe7644aa28600fa1ca653a64777d0937
kmu1205 <kmu1205@naver.com>
2017-05-18 17:24:10 +0900

    [에디터]

--- RunEditor.bat
+++ RunEditor.bat
@@ -1,2 +1,2 @@
-"%~dp0UE4Study\Engine\4.14.3\Engine\Binaries\Win64\UE4Editor.exe" "%~dp0UE4Study\Client\Client.uproject"
+"%~dp0\Engine\4.14.3\Engine\Binaries\Win64\UE4Editor.exe" "%~dp0\Client\Client.uproject"
 pause
